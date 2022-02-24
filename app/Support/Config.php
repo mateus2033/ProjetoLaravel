00b6961efd04ead075f5f46ae7044cc0be04f695
unknown <mateus2033@outlook.com>
2022-02-24 17:18:40 -0300

    Ajuste no envio de Email

--- app/Support/Config.php
+++ app/Support/Config.php
@@ -4,10 +4,10 @@ define("MAIL", [
 
     "host"=> "SMTP.office365.com",
     "port"=> "587",
-    "user"=> "mateus.pereira@aluno.faesa.br",
-    "password"   => "Sharon5378",
-    "from_name"  => "Mateus Pereira",
-    "from_email" => "mateus.pereira@aluno.faesa.br"
+    "user"=> "seuemail",
+    "password"   => "suasenha",
+    "from_name"  => "nome",
+    "from_email" => "seuemail"
 
 ]);
 
